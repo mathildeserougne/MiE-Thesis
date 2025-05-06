cea2bddc3977289a6e51f0d086e3120561bb5233
msxrougne <mathilde.serougne@ensae.fr>
2025-05-06 11:39:55 +0000

    adding the analysis for all sectors

--- Descriptive stats - India EU trade.R
+++ Descriptive stats - India EU trade.R
@@ -13,8 +13,8 @@ library(ggplot2)
 #### DATA WORK
 
 # importing the data
-baci <- read_csv("BACI_HS02_Y2019_V202501.csv")
-country_codes <- read_csv("country_codes_V202501.csv")
+baci <- read_csv("~/work/BACI_HS02_Y2019_V202501.csv")
+country_codes <- read_csv("~/work/country_codes_V202501.csv")
 
 baci <- baci %>%
   rename(
@@ -40,6 +40,51 @@ eu_members <- c(
   251, 300, 348, 372, 380, 428, 440, 442, 470, 528,
   616, 620, 203, 642, 703, 705, 752
 )
+
+
+## IMPORTANCE OF EUROPE AS A TRADE PARTNER, ALL SECTORS INCLUDED
+
+# indian exports only
+baci_total <- baci %>%
+  filter(exporter == 699) %>%
+  mutate(
+    destination = ifelse(importer %in% eu_members, "EU", as.character(importer_name))
+  )
+
+# aggregating all the flows per destinations (summing the products)
+total_exports_grouped <- baci_total %>%
+  group_by(destination) %>%
+  summarise(total_exports = sum(value, na.rm = TRUE), .groups = "drop")
+
+# top ten destinations
+top10_total <- total_exports_grouped %>%
+  slice_max(order_by = total_exports, n = 10) %>%
+  pull(destination)
+#  "Rest of the world"
+total_exports_grouped <- total_exports_grouped %>%
+  mutate(destination_grouped = ifelse(destination %in% top10_total, destination, "Rest of the world")) %>%
+  group_by(destination_grouped) %>%
+  summarise(total_exports = sum(total_exports), .groups = "drop")
+
+# percents (for clarity)
+total_exports_grouped <- total_exports_grouped %>%
+  mutate(
+    pct = total_exports / sum(total_exports),
+    label = paste0(round(pct * 100, 1), "%")
+  )
+
+# Camembert
+ggplot(total_exports_grouped, aes(x = "", y = total_exports, fill = destination_grouped)) +
+  geom_bar(stat = "identity", width = 1, color = "white") +
+  coord_polar(theta = "y") +
+  geom_text(aes(label = label),
+            position = position_stack(vjust = 0.5),
+            size = 3.5) +
+  labs(title = "2019 Indian exports in their main destinations (all sectors)",
+       fill = "Destination") +
+  theme_void()
+
+
 
 ## STEEL SECTOR AND IMPORTANCE OF EUROPE
 
@@ -87,8 +132,6 @@ ggplot(steel_exports_grouped, aes(x = "", y = total_exports, fill = destination_
   labs(title = "2019 Indian steel exports in their main destinations",
        fill = "Destination") +
   theme_void()
-
-
 
 
 
