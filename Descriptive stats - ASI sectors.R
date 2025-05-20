befb776fc7c6420e89186bbfc105a9365a2f8352
msxrougne <mathilde.serougne@ensae.fr>
2025-05-20 10:58:22 +0000

    creating file - asi sector stats

---
+++ Descriptive stats - ASI sectors.R
@@ -0,0 +1,20 @@
+#### PACKAGES
+
+library(dplyr)
+library(readr)
+library(stringr)
+library(ggplot2)
+library(readxl)
+library(tidyr)
+install.packages("writexl")
+library(writexl)
+library(scales)
+install.packages("zoo")  
+library(zoo)
+
+###############################################################################
+################### DESCRIPTIVE STATISTICS ####################################
+###############################################################################
+
+
+############# PRINCIPAL CHARACTERISTICS PER SECTOR ############################
